874345bed3093f74d1a1904cdee0758279727011
Frandam1 <cuentaamazon121@gmail.com>
2023-10-27 15:03:04 +0200

    Numero de resultados funcionando

--- arreglado/php/resultadostabla.php
+++ arreglado/php/resultadostabla.php
@@ -1,4 +1,20 @@
 <?php
+
+$mysqli = new mysqli("localhost", "registros", "registros", "registros");
+
+
+$sql = $_GET['sql']."";
+$result = $mysqli -> query($sql);
+$contador = 0;
+while ($row = $result -> fetch_assoc()) {
+    $contador++; 
+}
+echo '<p>La busqueda a devuelto '.$contador.' resultados</p>';
+?>
+
+<table colpadding = 0 colspacing=0 cellpadding=0 cellspacing=0 width=100% >
+
+<?php
 $mysqli = new mysqli("localhost", "registros", "registros", "registros");
 
 
