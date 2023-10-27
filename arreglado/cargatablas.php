974121401d008c8b2ad4ac48e804c002ebbf7786
Frandam1 <cuentaamazon121@gmail.com>
2023-10-27 10:38:14 +0200

    Peticion Basica funcionando

---
+++ arreglado/cargatablas.php
@@ -0,0 +1,12 @@
+<?php 
+$mysqli = new mysqli("localhost", "registros", "registros", "registros");
+
+$sql = "SHOW TABLES ";
+
+$result = $mysqli->query($sql);
+while ($row = $result->fetch_array()) {
+    echo '<option value="'.$row[0].'">'.$row[0].'</option>';
+}
+
+
+?>
